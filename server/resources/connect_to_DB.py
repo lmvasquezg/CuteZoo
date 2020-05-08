dceaf3aba5efbfca1a46e04ed83ce4db82db304b
Luisa Maria Vasquez Gomez <lmvasquezg@eafit.edu.co>
2020-05-08 18:16:15 -0500

    Update connect_to_DB.py

--- server/resources/connect_to_DB.py
+++ server/resources/connect_to_DB.py
@@ -2,6 +2,6 @@
 from pymongo import MongoClient
 
 # Connect to the database host given by MongoDB URI
-client = MongoClient('mongodb://lmvasquezg:ouyAYL58@cutezoo-shard-00-00-bxu9d.mongodb.net:27017,cutezoo-shard-00-01-bxu9d.mongodb.net:27017,cutezoo-shard-00-02-bxu9d.mongodb.net:27017/test?ssl=true&replicaSet=CuteZoo-shard-0&authSource=admin&retryWrites=true&w=majority')
+client = MongoClient('mongodb://lmvasquezg:admin@cutezoo-shard-00-00-bxu9d.mongodb.net:27017,cutezoo-shard-00-01-bxu9d.mongodb.net:27017,cutezoo-shard-00-02-bxu9d.mongodb.net:27017/test?ssl=true&replicaSet=CuteZoo-shard-0&authSource=admin&retryWrites=true&w=majority')
 # Access to the specific database
 db = client.CuteZoo
